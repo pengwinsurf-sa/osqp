9c87a5a57c12fa97f88719271d75cdadbd72722a
pengwinsurf-sa <ahmedzaki@devserver.us-central1-c.c.devservers-455014.internal>
2025-08-13 10:43:46 +0000

    check

--- include/private/version.h
+++ include/private/version.h
@@ -1,9 +1,3 @@
-/**
-This file is replaced by an auto-generated version.h
-with an OSQP_VERSION obtained from a variable supplied
-to cmake
-*/
-
 #ifndef OSQP_VERSION
-#define OSQP_VERSION "1.0.0"
+#define OSQP_VERSION "0.99"
 #endif
